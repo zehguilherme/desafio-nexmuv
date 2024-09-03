905c89e087f59b3512a228d3b3227074b63e6e68
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-09-03 20:14:58 -0300

    Adição da página de Login; Adição do react-router-dom e react-bootstrap

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/Login.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
